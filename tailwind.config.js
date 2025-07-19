e4cce85a13efac01b684b7a62a6ca63a18849a48
yashasrnair <yashasrnair@gmail.com>
2025-07-19 12:39:25 +0530

    css fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  purge: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths to match your project structure
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
